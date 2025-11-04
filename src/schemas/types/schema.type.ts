603e5cc54508999f1818ecd2e7e83baadfc8ded8
Aka-Amir <khaliliamir565@gmail.com>
2025-11-04 15:21:47 +0330

    Now schema's uses jsdoc and importing bugs are fixed !

--- src/schemas/types/schema.type.ts
+++ src/schemas/types/schema.type.ts
@@ -1,22 +1,36 @@
-import { EnumType } from './enum.type';
+import { EnumType } from "./enum.type";
 
 export type ObjectSchemaType = {
-  type: 'object';
+  type: "object";
   properties: Record<string, SchemaType>;
   required: string[];
+  description?: string;
+  example?: string;
 };
 
 export type ArraySchemaType = {
-  type: 'array';
+  type: "array";
   items: SchemaType;
+  description?: string;
+  example?: string;
 };
 
-export type EnumSchemaType = { type: string; enum?: EnumType; title?: string };
+export type EnumSchemaType = {
+  type: string;
+  enum?: EnumType;
+  title?: string;
+  description?: string;
+  example?: string;
+};
 
-export type SchemaRefType = { $ref: string };
-export type SchemaPropertyType =
-  | ObjectSchemaType
-  | ArraySchemaType
-  | EnumSchemaType;
+export type SchemaRefType = {
+  $ref: string;
+  description?: string;
+  example?: string;
+};
+export type SchemaPropertyType = {
+  description?: string;
+  example?: string;
+} & (ObjectSchemaType | ArraySchemaType | EnumSchemaType);
 
 export type SchemaType = SchemaRefType | SchemaPropertyType;
